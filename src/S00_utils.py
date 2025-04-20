d8116d75964138054f2512f52d40f7776af5e36a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-04-20 20:13:43 +0700

    Update

---
+++ src/S00_utils.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+
+def _residual(ax, y_pred, y, title, color="steelblue"):
+    ax.scatter(y_pred, y_pred - y, c=color, marker="o", edgecolor="white", s=70)
+    ax.hlines(y=0, xmin=-10, xmax=50, color="black", lw=2)
+    ax.axes.set_aspect("equal")
+    ax.set_title(title)
+
+
+def plot_res(y_train, y_test, y_train_pred, y_test_pred, xlim=[-2, 2]):
+    fig, ax = plt.subplots(
+        1, 2, figsize=(9, 3), constrained_layout=True, sharex=True, sharey=True
+    )
+    _residual(ax=ax[0], y_pred=y_train_pred, y=y_train, title="Train")
+    _residual(ax=ax[1], y_pred=y_test_pred, y=y_test, title="Test", color="limegreen")
+    ax[0].set_xlim(xlim)
+    fig.supxlabel("Predicted values")
+    fig.supylabel("Residual")
+
+
+def store_results(y_train, y_test, y_train_pred, y_test_pred):
+    MSE_train = mean_squared_error(y_train, y_train_pred)
+    MSE_test = mean_squared_error(y_test, y_test_pred)
+
+    R2_train = r2_score(y_train, y_train_pred)
+    R2_test = r2_score(y_test, y_test_pred)
+
+    data = {
+        "MSE Train": MSE_train,
+        "MSE Test": MSE_test,
+        "R2 Train": R2_train,
+        "R2 Test": R2_test,
+    }
+    return data
