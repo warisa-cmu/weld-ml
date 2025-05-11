bc1c15627f65e4e84ee01b4976e89784b61cbfb4
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-05-11 20:11:47 +0700

    Update

--- src/T02_fusion_transformer/S01_attempt_1/models.py
+++ src/T02_fusion_transformer/S01_attempt_1/models.py
@@ -123,3 +123,115 @@ class VariableSelectionNetwork(nn.Module):
         # Weighted sum of variable outputs
         weighted_output = (var_outputs * weights.unsqueeze(-1)).sum(dim=1)
         return weighted_output, weights  # (batch, hidden_dim), (batch, num_inputs)
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0, scale=True):
+        super(ScaledDotProductAttention, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.softmax = nn.Softmax(dim=2)
+        self.scale = scale
+
+    def forward(self, q, k, v, mask=None):
+        # print('---Inputs----')
+        # print('q: {}'.format(q[0]))
+        # print('k: {}'.format(k[0]))
+        # print('v: {}'.format(v[0]))
+
+        attn = torch.bmm(q, k.permute(0, 2, 1))
+        # print('first bmm')
+        # print(attn.shape)
+        # print('attn: {}'.format(attn[0]))
+
+        if self.scale:
+            dimention = torch.sqrt(torch.tensor(k.shape[-1]).to(torch.float32))
+            attn = attn / dimention
+        #    print('attn_scaled: {}'.format(attn[0]))
+
+        if mask is not None:
+            # fill = torch.tensor(-1e9).to(DEVICE)
+            # zero = torch.tensor(0).to(DEVICE)
+            attn = attn.masked_fill(mask == 0, -1e9)
+        #    print('attn_masked: {}'.format(attn[0]))
+
+        attn = self.softmax(attn)
+        # print('attn_softmax: {}'.format(attn[0]))
+        attn = self.dropout(attn)
+
+        output = torch.bmm(attn, v)
+
+        return output, attn
+
+
+class InterpretableMultiHeadAttention(nn.Module):
+    def __init__(self, n_head, d_model, dropout):
+        super(InterpretableMultiHeadAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_k = self.d_q = self.d_v = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.v_layer = nn.Linear(self.d_model, self.d_v, bias=False)
+        self.q_layers = nn.ModuleList(
+            [nn.Linear(self.d_model, self.d_q, bias=False) for _ in range(self.n_head)]
+        )
+        self.k_layers = nn.ModuleList(
+            [nn.Linear(self.d_model, self.d_k, bias=False) for _ in range(self.n_head)]
+        )
+        self.v_layers = nn.ModuleList([self.v_layer for _ in range(self.n_head)])
+        self.attention = ScaledDotProductAttention()
+        self.w_h = nn.Linear(self.d_v, self.d_model, bias=False)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if "bias" not in name:
+                torch.nn.init.xavier_uniform_(p)
+            #                 torch.nn.init.kaiming_normal_(p, a=0, mode='fan_in', nonlinearity='sigmoid')
+            else:
+                torch.nn.init.zeros_(p)
+
+    def forward(self, q, k, v, mask=None):
+        heads = []
+        attns = []
+        for i in range(self.n_head):
+            qs = self.q_layers[i](q)
+            ks = self.k_layers[i](k)
+            vs = self.v_layers[i](v)
+            # print('qs layer: {}'.format(qs.shape))
+            head, attn = self.attention(qs, ks, vs, mask)
+            # print('head layer: {}'.format(head.shape))
+            # print('attn layer: {}'.format(attn.shape))
+            head_dropout = self.dropout(head)
+            heads.append(head_dropout)
+            attns.append(attn)
+
+        head = torch.stack(heads, dim=2) if self.n_head > 1 else heads[0]
+        # print('concat heads: {}'.format(head.shape))
+        # print('heads {}: {}'.format(0, head[0,0,Ellipsis]))
+        attn = torch.stack(attns, dim=2)
+        # print('concat attn: {}'.format(attn.shape))
+
+        outputs = torch.mean(head, dim=2) if self.n_head > 1 else head
+        # print('outputs mean: {}'.format(outputs.shape))
+        # print('outputs mean {}: {}'.format(0, outputs[0,0,Ellipsis]))
+        outputs = self.w_h(outputs)
+        outputs = self.dropout(outputs)
+
+        return outputs, attn
+
+
+embedding_size = 12
+d_model = embedding_size
+n_head = 4
+batch_size = 12
+number_features = 6
+
+att_layer = InterpretableMultiHeadAttention(n_head=n_head, d_model=d_model, dropout=0.5)
+x = torch.randn(batch_size, number_features, embedding_size)
+print(x.shape)
+output, att = att_layer(x, x, x)
+print(output.shape)
+print(att.shape)
