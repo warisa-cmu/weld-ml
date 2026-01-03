ad83ef37daecdd84c8cb5723b63d8c55c3fbdfc4
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-01-03 09:16:54 +0700

    Add machine learning model training and evaluation scripts
    - Implemented S01_run.py for training multiple regression models using GridSearchCV.
    - Added functionality to load data, preprocess, and perform hyperparameter tuning.
    - Created S02_analyze.py for evaluating model performance with residual plots and CV results visualization.
    - Integrated data saving features for both training results and evaluation metrics.

--- src/P04_ml/T00_lib/classes.py
+++ src/P04_ml/T00_lib/classes.py
@@ -1,8 +1,16 @@
 import pickle
 from datetime import datetime
 
+import pandas as pd
 from sklearn.base import BaseEstimator
+from sklearn.metrics import (
+    PredictionErrorDisplay,
+    mean_absolute_percentage_error,
+    mean_squared_error,
+    r2_score,
+)
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 class MyUtil:
@@ -61,3 +69,104 @@ class RegSwitcher(BaseEstimator):
 
     def predict(self, X):
         return self.base.predict(X)
+
+
+class MyEval:
+    @staticmethod
+    def eval_perf(y_true, y_pred):
+        mse = mean_squared_error(y_true=y_true, y_pred=y_pred)
+        mape = mean_absolute_percentage_error(y_true=y_true, y_pred=y_pred)
+        r2 = r2_score(y_true=y_true, y_pred=y_pred)
+        return mse, mape, r2
+
+    @staticmethod
+    def pp():
+        print("no")
+
+    @classmethod
+    def eval(cls, Y_train, Y_test, Y_train_pred, Y_test_pred, **kwargs):
+        data_arr = []
+        for i in range(0, Y_train.shape[1]):
+            mse_train, mape_train, r2_train = cls.eval_perf(
+                y_true=Y_train[:, i], y_pred=Y_train_pred[:, i]
+            )
+            mse_test, mape_test, r2_test = cls.eval_perf(
+                y_true=Y_test[:, i], y_pred=Y_test_pred[:, i]
+            )
+
+            data = {
+                **kwargs,
+                "Y": f"Y-{i + 1}",
+                "MSE Train (No Val)": mse_train,
+                "MSE Test": mse_test,
+                "MAPE Train (No Val)": mape_train,
+                "MAPE Test": mape_test,
+                "R2 Train (No Val)": r2_train,
+                "R2 Test": r2_test,
+            }
+            data_arr.append(data)
+
+        mse_train, mape_train, r2_train = cls.eval_perf(
+            y_true=Y_train, y_pred=Y_train_pred
+        )
+        mse_test, mape_test, r2_test = cls.eval_perf(y_true=Y_test, y_pred=Y_test_pred)
+        data = {
+            **kwargs,
+            "Y": "Y-All",
+            "MSE Train (No Val)": mse_train,
+            "MSE Test": mse_test,
+            "MAPE Train (No Val)": mape_train,
+            "MAPE Test": mape_test,
+            "R2 Train (No Val)": r2_train,
+            "R2 Test": r2_test,
+        }
+        data_arr.append(data)
+        df_eval = pd.DataFrame.from_dict(data_arr)
+        return df_eval
+
+    @classmethod
+    def plot_res(
+        cls,
+        Y_train,
+        Y_test,
+        Y_train_pred,
+        Y_test_pred,
+        dt="",
+        save=False,
+        show=True,
+        file_prefix="",
+    ):
+        for i in range(0, Y_train.shape[1]):
+            fig, axes = plt.subplots(
+                nrows=1,
+                ncols=2,
+                figsize=(10, 5),
+                constrained_layout=True,
+                sharex=True,
+                sharey=True,
+            )
+
+            display_train = PredictionErrorDisplay(
+                y_true=Y_train[:, i], y_pred=Y_train_pred[:, i]
+            )
+            display_train.plot(ax=axes[0])
+            axes[0].set_title("Train")
+
+            display_train = PredictionErrorDisplay(
+                y_true=Y_test[:, i], y_pred=Y_test_pred[:, i]
+            )
+            display_train.plot(ax=axes[1])
+            axes[1].set_title("Test")
+
+            if file_prefix != "":
+                fig.suptitle(file_prefix)
+
+            if save:
+                if file_prefix == "":
+                    raise Exception("Please specify file prefix")
+
+                filename = f"{file_prefix}_{dt}_{i}.png"
+                fig.savefig(filename, dpi=300)
+
+            if show:
+                plt.show()
---
+++ src/P04_ml/T01_ml/S01_run.py
@@ -0,0 +1,121 @@
+# %%
+import re
+from pathlib import Path
+from pprint import pp
+
+import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV, ParameterGrid
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+
+from P04_ml.T00_lib.classes import DataHandler, MyUtil, RegSwitcher
+from P04_ml.T00_lib.utils import check_jupyter
+
+if check_jupyter():
+    BASE_DIR = Path.cwd()  # Current directory of the running file
+    DATA_DIR = BASE_DIR.parent.parent / "P03_data_preprocess"
+    CURRENT_DIR = BASE_DIR
+else:
+    BASE_DIR = Path.cwd()  # Base directory of the project
+    DATA_DIR = BASE_DIR / "src/P03_data_preprocess"
+    CURRENT_DIR = Path(__file__).resolve().parent
+
+SAVE_DATA = True
+dt = MyUtil.get_dt()
+print(f"Current Directory: {CURRENT_DIR}")
+print(f"Current Date and Time: {dt}")
+
+# Load data
+df = pd.read_excel(DATA_DIR / "S07_data_combined_loc.xlsx")
+print(df.shape)
+
+# %%
+# Extract features and targets
+_X = df.iloc[:, :-3].values
+_Y = df.iloc[:, -3:].values
+print(_X.shape)
+print(_Y.shape)
+
+# Create DataHandler instance
+data_handler = DataHandler(
+    _X=_X, _Y=_Y, scalerX=StandardScaler(), scalerY=StandardScaler()
+)
+
+# Create parameter grid
+param_grid_split = [{"random_state": [1, 2, 3, 4, 5], "test_size": [0.3]}]
+param_list_split = list(ParameterGrid(param_grid_split))
+pp(param_list_split)
+base_lr = MultiOutputRegressor(estimator=LinearRegression())
+base_svr = MultiOutputRegressor(estimator=SVR())
+base_rf = MultiOutputRegressor(estimator=RandomForestRegressor())
+base_gbr = MultiOutputRegressor(estimator=GradientBoostingRegressor())
+
+param_grid_hyper = [
+    {"base": [base_lr]},
+    {"base": [base_svr], "base__estimator__C": [0.01, 0.1, 1]},
+    {"base": [base_rf], "base__estimator__n_estimators": [10, 50, 200]},
+    {"base": [base_gbr], "base__estimator__max_depth": [1, 2, 3, 4, 5]},
+]
+
+# Initialize blank model (optional)
+reg = RegSwitcher(base=None)
+
+df_arr = []
+for idx_split, param_split in enumerate(param_list_split):
+    data_handler.split_and_scale(**param_split)
+    X_train, Y_train = data_handler.get_train()
+
+    gs = GridSearchCV(
+        estimator=reg,
+        param_grid=param_grid_hyper,
+        cv=3,
+        # scoring="neg_mean_squared_error",
+        scoring="r2",
+        n_jobs=-1,
+    )
+    gs.fit(X_train, Y_train)
+    _df = pd.DataFrame(gs.cv_results_)
+    _df["id_split"] = idx_split
+    _df["param_split"] = [param_split for _ in range(_df.shape[0])]
+    df_arr.append(_df)
+
+df_cv = pd.concat(df_arr)
+df_cv = df_cv.reset_index().rename(columns={"index": "id_gs"})
+
+# Process results
+df_cv["estimator"] = df_cv["param_base"].apply(lambda x: x.estimator.__class__.__name__)
+pattern = r"split\d+_test_score"
+colsSplitTestScore = [col for col in df_cv.columns if re.fullmatch(pattern, col)]
+df_cv["validation_scores"] = df_cv[colsSplitTestScore].apply(
+    lambda row: row.values, axis=1
+)
+cols = [
+    "id_split",
+    "param_split",
+    "id_gs",
+    "params",
+    "estimator",
+    "mean_test_score",
+    "std_test_score",
+    "rank_test_score",
+    "validation_scores",
+]
+df_cv = df_cv[cols]
+df_cv
+
+if SAVE_DATA:
+    filename = CURRENT_DIR / f"S01_{dt}.pkl"
+
+    data_save = {
+        "desc": "Results of ML model selection using GridSearchCV",
+        "data_handler": data_handler,
+        "param_split": param_split,
+        "param_grid_hyper": param_grid_hyper,
+        "df_cv": df_cv,
+    }
+
+    # Save the model
+    MyUtil.save_data(filename=filename, data=data_save)
