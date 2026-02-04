442a83ba8b359860748b5417107d6e4ca827e6f0
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-02-04 07:14:21 +0700

    Update

--- run1/lib/shap.py
+++ run1/lib/shap.py
@@ -3,13 +3,20 @@ import shap
 
 class ShapUtil:
     @staticmethod
-    def get_shap_explainer(model_name, estimator, X):
+    def get_shap_explainer(model_name, estimator, X, check_additivity=True):
         if model_name in ["RFR", "DTR", "GBR", "XGBR"]:
-            explainer = shap.TreeExplainer(estimator, X)
+            # explainer = shap.TreeExplainer(
+            #     estimator, X, check_additivity=check_additivity
+            # )
+            explainer = shap.Explainer(
+                estimator.predict, X, check_additivity=check_additivity
+            )
         # elif model_name in ["EN"]:
         #     explainer = shap.LinearExplainer(estimator, X)
         elif model_name in ["EN", "SVR", "KNR"]:
-            explainer = shap.Explainer(estimator.predict, X)
+            explainer = shap.Explainer(
+                estimator.predict, X, check_additivity=check_additivity
+            )
         else:
             raise ValueError(f"Unknown model_name: {model_name}")
         return explainer
